b31cf1ca5d134264607ac554211bd371880b0778
louisljh-wb23-stack <louisljh-wb23@student.tarc.edu.my>
2026-08-20 10:34:42 +0800

    Update bustype.sql

--- bustype.sql
+++ bustype.sql
@@ -2,5 +2,5 @@
 INSERT INTO bus_type VALUES ('BT001', 'STANDARD SCHOOL BUS', 'Basic school/campus shuttle');
 INSERT INTO bus_type VALUES ('BT002', 'DELUXE BUS',          'Charter Bus + Premium comfort coach, television, and free food');
 INSERT INTO bus_type VALUES ('BT003', 'CHARTER BUS',         'Available for long-distance travel/tourism');
-INSERT INTO bus_type VALUES ('BT004', 'MAGIC BUS',           'Muggle can't see');
+INSERT INTO bus_type VALUES ('BT004', 'MAGIC BUS',           'Muggle can''t see');
 
